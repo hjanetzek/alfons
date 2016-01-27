6a323d82e4445cf60d45cfeb8a32b90a0a36cd05
Hannes Janetzek <hannes.janetzek@gmail.com>
2016-01-27 06:17:22 +0100

    add get_deps.sh
    - with submodules other projects including this
      could checkout 'glm' twice when doing git clone --recursive

---
+++ get_deps.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+# TODO check if dir exists, update to fixed revisions
+
+git clone --recursive https://github.com/tangrams/harfbuzz-icu-freetype deps/hb-icu-ft
+git clone https://github.com/g-truc/glm.git deps/glm
