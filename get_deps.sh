ad0dc61972f59259c78a15913d09331705e51e73
Hannes Janetzek <hannes.janetzek@gmail.com>
2016-08-29 16:02:10 +0200

    remove hb-icu-ft submodule

--- get_deps.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# TODO check if dir exists, update to fixed revisions
-
-git clone --recursive https://github.com/tangrams/harfbuzz-icu-freetype deps/hb-icu-ft
-git clone https://github.com/g-truc/glm.git deps/glm
-git clone https://github.com/memononen/nanovg deps/nanovg
